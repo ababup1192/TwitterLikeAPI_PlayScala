978a950e99c99235561797606aaa36519e6ed4ca
ababup1192 <ababup1192@gmail.com>
2015-07-01 23:14:50 +0900

    Introduce scala test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,13 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
-class Application extends Controller {
+trait Application {
+  this: Controller =>
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Ok")
   }
-
 }
+
+object Application extends Controller with Application
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,30 +1,27 @@
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
-
-import play.api.test._
+import controllers.Application
+import org.scalatestplus.play._
+import play.api.mvc._
 import play.api.test.Helpers._
+import play.api.test._
+
+import scala.concurrent.Future
 
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
-@RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends PlaySpec with Results {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
-    }
+    "return response" in {
+      class TestController() extends Controller with Application
 
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      val controller = new TestController()
+      val result: Future[Result] = controller.index().apply(FakeRequest())
+      val bodyText: String = contentAsString(result)
+      bodyText mustBe "Ok"
     }
   }
 }
